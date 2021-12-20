9a66e83a2dd9af43c01a8efa0d28651d3cb11a4b
gshrdr <shrader.gavin@gmail.com>
2021-12-20 14:29:32 -0500

    Working on the placement/CSS of the google-maps component of my contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -40,26 +40,21 @@ export default function Contact() {
             src="https://www.google.com/maps/embed/v1/place?q=Richmond,+Virginia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
+            <div className="lg:w-1/2 px-6 mt-1 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
+                LOCATION
               </h2>
               <p className="mt-1">
-                xxxxxxxx <br />
-                xxxxxxxx
+                Richmond, VA
               </p>
             </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+            <div className="lg:w-1/2 px-6 mt-2 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                email
+                shrader.gavin@gmail.com
               </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE
-              </h2>
-              <p className="leading-relaxed">xxx-xxx-xxxx</p>
             </div>
           </div>
         </div>
