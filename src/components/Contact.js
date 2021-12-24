3eee6ffd51bbb734de9c8c330b64bc5a938b6974
gshrdr <shrader.gavin@gmail.com>
2021-12-23 21:43:26 -0500

    Fixed layout of Contact title

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap justify-center">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
@@ -57,9 +57,11 @@ export default function Contact() {
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Contact
-          </h2>
+          <div className="flex justify-center md:justify-start">
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+              Contact
+            </h2>
+          </div>
           <p className="leading-relaxed mb-5">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
             suscipit officia aspernatur veritatis. Asperiores, aliquid?
