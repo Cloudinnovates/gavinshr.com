4e1e168a955321d565e9d4bae79f6c25f2d15983
gshrdr <shrader.gavin@gmail.com>
2022-02-08 15:05:46 -0500

    Updating homepage formatting

--- src/Routes/Homepage/Resume.js
+++ src/Routes/Homepage/Resume.js
@@ -2,6 +2,7 @@ export default function Resume() {
   return (
     <div id="resume">
       <hr/>
+      <div className="center">
         <h1>Resume</h1>
 
         <p style={{ paddingTop: "15px" }}>This is the most up to date version of my resume. I am continuing to update & improve this with the goal of becoming a software engineer & web developer.</p>
@@ -9,6 +10,7 @@ export default function Resume() {
         <div style={{ paddingTop: "15px" }}>
           <a className="link" href="/Resume.html" target="_blank" rel="noopener noreferrer">View Resume &rarr;</a>
         </div>
+      </div>
     </div>
   );
 }
