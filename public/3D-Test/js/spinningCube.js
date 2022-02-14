34102715c17a35ba8d359702467e1ab3abdff8ff
gshrdr <shrader.gavin@gmail.com>
2022-02-13 22:06:33 -0500

    Updated three js files

--- public/3D-Test/js/spinningCube.js
+++ public/3D-Test/js/spinningCube.js
@@ -5,29 +5,30 @@ init();
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
 	camera.position.z = 1;
 
 	scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.4, 0.4, 0.4 );
+	geometry = new THREE.BoxGeometry(0.35, 0.35, 0.35);
 	material = new THREE.MeshNormalMaterial();
 
-	mesh = new THREE.Mesh( geometry, material );
+	mesh = new THREE.Mesh(geometry, material);
 	scene.add( mesh );
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
-	document.body.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setAnimationLoop(animation);
+	document.body.appendChild(renderer.domElement);
 
 }
 
 function animation( time ) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	mesh.rotation.x = time / 2500;
+	mesh.rotation.y = time / 2500;
+	mesh.rotation.z = time / 2500;
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 
 }
