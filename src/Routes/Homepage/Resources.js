c6eb2a6b3d6d3a8f87b0f465f5b2695b2f3d4b4e
gshrdr <shrader.gavin@gmail.com>
2022-03-02 10:08:21 -0500

    Updated resources & read me files

--- src/Routes/Homepage/Resources.js
+++ src/Routes/Homepage/Resources.js
@@ -12,21 +12,21 @@ export default function Resources() {
       <div style={{ paddingTop: "15px" }}><Link className="link" to="/">&larr; Homepage</Link></div>
 
       <p style={{ paddingTop: "15px" }}>These are the great resources & assets that were used to assist in the creation of this site, thank you!</p>
-      <div style={{ paddingTop: "15px" }}>
-        <p><a className="link" href="https://www.freecodecamp.org/news/build-portfolio-website-react/" target="_blank" rel="noopener noreferrer">ReactJS Portfolio Design Inspiration + Tutorial</a></p>
-        <p><a className="link" href="https://jfelix.info/blog/create-a-mobile-friendly-navigation-with-react" target="_blank" rel="noopener noreferrer">ReactJS Mobile Friendly Menu Inspiration + Tutorial</a></p>
-        <p><a className="link" href="https://www.vecteezy.com/free-vector/astronaut" target="_blank" rel="noopener noreferrer">Astronaut Vectors by Vecteezy</a></p>
-        <p><a className="link" href="https://www.vecteezy.com/free-vector/404" target="_blank" rel="noopener noreferrer">404 Vectors by Vecteezy</a></p>
-        <p><a className="link" href="https://codepen.io/keithclark/pen/ibEnk" target="_blank" rel="noopener noreferrer">CSS Starfield Animation</a></p>
-        <p><a className="link" href="https://fontawesome.com/v4.7/icons/" target="_blank" rel="noopener noreferrer">Font-Awesome Icon Pack</a></p>
-        <p><a className="link" href="https://tailwindcss.com/docs/customizing-colors" target="_blank" rel="noopener noreferrer">Tailwind CSS Color Schemes</a></p>
-        <p><a className="link" href="https://zellwk.com/about/" target="_blank" rel="noopener noreferrer">Link Hover Animation Inspiration - Zell Liew</a></p>
-        <p><a className="link" href="https://www.pluralsight.com/guides/re-render-react-component-on-window-resize" target="_blank" rel="noopener noreferrer">ReactJS - Handle Re-Rendering Components With Window Resize</a></p>
-        <p><a className="link" href="https://mailtrap.io/blog/react-contact-form/" target="_blank" rel="noopener noreferrer">ReactJS + PHP Email Based Contact Form</a></p>
-        <p><a className="link" href="https://favicon.io" target="_blank" rel="noopener noreferrer">Favicon Generator</a></p>
-        <p><a className="link" href="https://cloudconvert.com/pdf-to-html" target="_blank" rel="noopener noreferrer">PDF To HTML Converter</a></p>
-        <p><a className="link" href="https://codewithawa.com/posts/how-to-create-a-blog-in-php-and-mysql-database" target="_blank" rel="noopener noreferrer">PHP & MySQL Blog Tutorial</a></p>
-      </div>
+      <ul style={{ paddingTop: "15px", paddingLeft: "15px", paddingRight: "15px" }}>
+        <li><a className="link" href="https://www.freecodecamp.org/news/build-portfolio-website-react/" target="_blank" rel="noopener noreferrer">ReactJS Portfolio Design Inspiration + Tutorial</a></li>
+        <li><a className="link" href="https://jfelix.info/blog/create-a-mobile-friendly-navigation-with-react" target="_blank" rel="noopener noreferrer">ReactJS Mobile Friendly Menu Inspiration + Tutorial</a></li>
+        <li><a className="link" href="https://www.vecteezy.com/free-vector/astronaut" target="_blank" rel="noopener noreferrer">Astronaut Vectors by Vecteezy</a></li>
+        <li><a className="link" href="https://www.vecteezy.com/free-vector/404" target="_blank" rel="noopener noreferrer">404 Vectors by Vecteezy</a></li>
+        <li><a className="link" href="https://codepen.io/keithclark/pen/ibEnk" target="_blank" rel="noopener noreferrer">CSS Starfield Animation</a></li>
+        <li><a className="link" href="https://fontawesome.com/v4.7/icons/" target="_blank" rel="noopener noreferrer">Font-Awesome Icon Pack</a></li>
+        <li><a className="link" href="https://tailwindcss.com/docs/customizing-colors" target="_blank" rel="noopener noreferrer">Tailwind CSS Color Schemes</a></li>
+        <li><a className="link" href="https://zellwk.com/about/" target="_blank" rel="noopener noreferrer">Link Hover Animation Inspiration - Zell Liew</a></li>
+        <li><a className="link" href="https://www.pluralsight.com/guides/re-render-react-component-on-window-resize" target="_blank" rel="noopener noreferrer">ReactJS - Handle Re-Rendering Components With Window Resize</a></li>
+        <li><a className="link" href="https://mailtrap.io/blog/react-contact-form/" target="_blank" rel="noopener noreferrer">ReactJS + PHP Email Based Contact Form</a></li>
+        <li><a className="link" href="https://favicon.io" target="_blank" rel="noopener noreferrer">Favicon Generator</a></li>
+        <li><a className="link" href="https://cloudconvert.com/pdf-to-html" target="_blank" rel="noopener noreferrer">PDF To HTML Converter</a></li>
+        <li><a className="link" href="https://codewithawa.com/posts/how-to-create-a-blog-in-php-and-mysql-database" target="_blank" rel="noopener noreferrer">PHP & MySQL Blog Tutorial</a></li>
+      </ul>
 
       <div style={{ width: "100%", height: "300px" }}/>
     </div>
