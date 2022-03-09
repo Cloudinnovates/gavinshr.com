f5270451babb24e9db4558b23e2f7da4ac0f59e1
gshrdr <shrader.gavin@gmail.com>
2022-03-08 22:27:36 -0500

    Working on laying out BlogData & Blog css files so I can begin showcasing links to Medium articles on my website

--- src/Routes/Blog/Blog.js
+++ src/Routes/Blog/Blog.js
@@ -1,4 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom"
+import { HashLink } from "react-router-hash-link"
+import { posts } from "./BlogData";
+import "./Blog.css"
 
 export default function Blog() {
   return (
@@ -6,13 +9,54 @@ export default function Blog() {
       <div className="container subscreen-container">
         <i className="fa fa-book fa-2x" style={{ display: "inline-block", marginRight: "10px" }}></i>
         <h1 style={{ display: "inline-block" }}>Blog</h1>
-
         <p style={{ paddingTop: "25px" }}>Welcome to the blog page.</p>
-        <div style={{ paddingTop: "15px" }}><Link className="link" to="/">&larr; Homepage</Link></div>
-
         <p style={{ paddingTop: "15px" }}>This blog is a work in progress. Expect to see changes and updates in the near future!</p>
+        <div style={{ paddingTop: "15px", paddingBottom: "2rem" }}><Link className="link" to="/">&larr; Homepage</Link></div>
 
-        <div style={{ width: "100%", height: "800px" }}/>
+        <hr/>
+        <br/>
+        <h2>Most Recent Posts</h2>
+        <h3><i>Work In Progress</i></h3>
+        <div className="blog-container">
+          {posts.map((post) => (
+            <div key={post.skill} className="blog-item">
+              <span>
+                <div className="blog-title-container">
+                  <div style={{ backgroundColor: post.color }} className="skills-color"></div>
+                  <h2 className="skills-title">{post.skill}</h2>
+                </div>
+                <h3 className="blog-subheading"><i>{post.subheading}</i></h3>
+              </span>
+
+              <span>
+                {
+                  post.moreInfo == null ? <br/> :
+                  <p className="blog-item-subsection">{post.moreInfo}</p>
+                }
+              </span>
+              <span>
+                <div className="blog-item-subsection">
+                {
+                  post.url == null ? ""
+                  :
+                    post.urlIsNavLink === true ?
+                      <NavLink className="link" to={post.url}>{post.urlName}</NavLink>
+                      :
+                        post.urlIsHashLink === true ?
+                          <HashLink className="link" to={post.url}>{post.urlName}</HashLink>
+                          :
+                          post.urlOpensInNewTab === true ?
+                            <a className="link" href={post.url} target="_blank" rel="noopener noreferrer">{post.urlName}</a>
+                            :
+                            <a className="link" href={post.url}>{post.urlName}</a>
+                }
+                </div>
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div style={{ width: "100%", height: "600px" }}/>
       </div>
     </main>
   );
---
+++ src/Routes/Blog/BlogData.js
@@ -0,0 +1,17 @@
+/*
+ * Blog Posts
+ */
+
+export const posts = [
+  {
+    skill: "Post 1",
+    subheading: "Temporary Testing Data",
+    color: "#16a34a",
+    url: "",
+    urlName: "Read Blog Post",
+    urlOpensInNewTab: true,
+    urlIsNavLink: false,
+    urlIsHashLink: false,
+    moreInfo: "This is temporary testing data and will be replaced with a real blog post in the near future once I finish building this section.",
+  }
+]
